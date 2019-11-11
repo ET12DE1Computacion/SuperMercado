ab09a9d3ad418b3ad189ab56a43de37f26fba6ca
luchoxx87 <neoluis87@gmail.com>
2019-11-10 22:48:13 -0300

    actualizados scripts de grants del gerente

--- ProgramaGerente/AdoGerente.cs
+++ ProgramaGerente/AdoGerente.cs
@@ -2,8 +2,14 @@
 
 namespace ProgramaGerente
 {
+    /// <summary>
+    /// Clase estatica que proporcionado el ADO seteado para un gerente
+    /// </summary>
     public static class AdoGerente
     {
+        /// <summary>
+        /// Propiedad para acceso al ADO
+        /// </summary>
         public static IADO ADO { get; set; } =
             FactoryAdoMySQL.GetAdoDesdeJson("appsettings.json", "gerente");
     }
--- scripts/bd/MySQL/Users y Grants.sql
+++ scripts/bd/MySQL/Users y Grants.sql
@@ -1,6 +1,9 @@
+# Creacion de Usuarios
 CREATE USER IF NOT EXISTS 'supermercado'@'localhost' IDENTIFIED BY 'supermercado';
 CREATE USER IF NOT EXISTS 'cajero'@'10.120.0.%' IDENTIFIED BY 'passCajero';
 CREATE USER IF NOT EXISTS 'gerenteSuper'@'localhost' IDENTIFIED BY 'passGerente';
 
-GRANT ALL ON Supermercado.* TO 'supermercado'@'localhost';
+# Grants gerenteSuper
 GRANT SELECT, INSERT on Categoria to 'gerenteSuper'@'localhost';
+GRANT SELECT, INSERT, UPDATE(nombre, cantidad, precioUnitario) on Producto to 'gerenteSuper'@'localhost';
+GRANT SELECT, INSERT on HistorialPrecio to 'gerenteSuper'@'localhost';
