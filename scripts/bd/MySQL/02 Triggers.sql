e47c592c4362385de1f89eafc8835a2d2b499895
luchoxx87 <neoluis87@gmail.com>
2021-10-20 04:15:10 -0300

    Update 02 Triggers.sql

--- scripts/bd/MySQL/02 Triggers.sql
+++ scripts/bd/MySQL/02 Triggers.sql
@@ -11,3 +11,14 @@ BEGIN
         CALL altaIngresoStock (NEW.idProducto, NEW.cantidad);
     END IF;
 END $$
+
+CREATE TRIGGER aftUpdProducto AFTER UPDATE ON Producto
+FOR EACH ROW
+BEGIN
+    IF (OLD.precioUnitario != NEW.precioUnitario) THEN
+        CALL altaHistorialPrecio (NEW.idProducto, NEW.precioUnitario);
+    END IF;
+    IF (NEW.cantidad > OLD.cantidad) THEN
+        CALL altaIngresoStock (NEW.idProducto, NEW.cantidad - OLD.cantidad);
+    END IF;
+END
