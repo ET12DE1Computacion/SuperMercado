cf62daf8532b369bc366ca19bdd1012aa3761b91
Luis Duran <neoluis87@gmail.com>
2023-10-18 17:03:05 -0300

    Agregando mapeo producto

--- src/cSharp/Super.Core/IAdo.cs
+++ src/cSharp/Super.Core/IAdo.cs
@@ -4,4 +4,7 @@ public interface IAdo
 {
     void AltaCategoria(Categoria categoria);
     List<Categoria> ObtenerCategorias();
+    void AltaProducto(Producto producto);
+    List<Producto> ObtenerProductos();
+    Producto? ObtenerProducto(short id);
 }
--- src/cSharp/Super.Dapper/AdoDapper.cs
+++ src/cSharp/Super.Dapper/AdoDapper.cs
@@ -9,6 +9,10 @@ public class AdoDapper : IAdo
     private readonly IDbConnection _conexion;
     private readonly string _queryCategorias
         = "SELECT idRubro AS id, rubro AS nombre FROM Rubro";
+    private readonly string _queryProductos
+        = @"SELECT  idProducto AS 'Producto.id', nombre, precioUnitario, cantidad, Producto.idRubro AS 'Categoria.id', rubro AS 'Categoria.nombre'
+            FROM    Producto
+            JOIN    Rubro USING (idRubro)";
 
     public AdoDapper(IDbConnection conexion) => this._conexion = conexion;
     
@@ -27,6 +31,22 @@ public class AdoDapper : IAdo
         //Obtengo el valor de parametro de tipo salida
         categoria.Id = parametros.Get<byte>("@unIdRubro");
     }
+
+    public void AltaProducto(Producto producto)
+    {
+        throw new NotImplementedException();
+    }
+
     public List<Categoria> ObtenerCategorias()
         => _conexion.Query<Categoria>(_queryCategorias).ToList();
+
+    public Producto? ObtenerProducto(short id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Producto> ObtenerProductos()
+    {
+        return _conexion.Query<Producto>(_queryProductos).ToList();
+    }
 }
