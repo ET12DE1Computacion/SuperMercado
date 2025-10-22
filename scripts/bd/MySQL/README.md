a2db72ffd8da752f1b25c55a9cbd64b626127fc2
Luis Duran <neoluis87@gmail.com>
2025-10-22 11:49:16 -0300

    Refactor en frmAltaProducto

---
+++ scripts/bd/MySQL/README.md
@@ -0,0 +1,15 @@
+## ¿Como volcar estos scripts en tu SGBD 🐬?
+
+Hay que abrir la terminal en este directorio (`scripts\bd\MySQL\`) y escriban en la terminal
+
+```bash
+mysql -u tuUsuarioMySQL -p
+```
+
+y luego escribir el comando
+
+```bash
+mysql> SOURCE install.sql
+```
+
+Y con eso corren los scripts en su SGBD 🐬 ✌🏽
--- src/cSharp/Super.Winf/frmAltaProducto.cs
+++ src/cSharp/Super.Winf/frmAltaProducto.cs
@@ -7,14 +7,16 @@ namespace Super.Winf;
 public partial class FrmAltaProducto : Form
 {
     //Propiedad que representa el objeto mediante el cual voy a leer/escribir en la BD
-    IRepoProducto repoProducto;
+    readonly IRepoProducto repoProducto;
     public FrmAltaProducto()
     {
         var cadena = "Server=localhost;User ID=5to_agbd;Password=Trigg3rs!;Database=5to_Supermercado;";
         var conector = new MySqlConnection(cadena);
-        
+
         //Instancio el objeto como un acceso a nuestro Repo en Dapper
         repoProducto = new RepoProducto(conector);
+        
+        //Es importante que este metodo este al final
         InitializeComponent();
     }
 
@@ -36,17 +38,7 @@ public partial class FrmAltaProducto : Form
     {
         try
         {
-            if (lstCategorias.SelectedItem is null)
-                throw new InvalidOperationException("Seleccione primero una categoria");
-
-            //Como el elemento seleccionado siempre es de tipo object, lo "transformo" (cast) a Categoria
-            var categoria = lstCategorias.SelectedItem as Categoria;
-            if (categoria is null)
-                throw new InvalidOperationException("Seleccione una categoria válida");
-            var nombre = txtNombre.Text;
-            var precio = numPrecio.Value;
-            var stock = (ushort)numStock.Value;
-            var producto = new Producto(categoria, nombre, precio, stock);
+            var producto = ObtenerProductoDesdeForm();
 
             //Fijense como con el objeto ya instanciado, se lo paso al ado/repo para que lo inserte en la BD
             repoProducto.AltaProducto(producto);
@@ -58,6 +50,21 @@ public partial class FrmAltaProducto : Form
 
             throw;
         }
+    }
+
+    private Producto ObtenerProductoDesdeForm()
+    {
+        if (lstCategorias.SelectedItem is null)
+            throw new InvalidOperationException("Seleccione primero una categoria");
+
+        //Como el elemento seleccionado siempre es de tipo object, lo "transformo" (cast) a Categoria
+        var categoria = lstCategorias.SelectedItem as Categoria;
+        if (categoria is null)
+            throw new InvalidOperationException("Seleccione una categoria válida");
+        var nombre = txtNombre.Text;
+        var precio = numPrecio.Value;
+        var stock = (ushort)numStock.Value;
+        return new Producto(categoria, nombre, precio, stock);
     }
 
     private void btnCrear_Click(object sender, EventArgs e) => CrearProducto();
